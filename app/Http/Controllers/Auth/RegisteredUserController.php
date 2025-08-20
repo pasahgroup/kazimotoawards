9b3da29ffbfc510a61f9393f3022968404d96526
pasahgroup <info@pasah.net>
2025-08-20 11:17:48 +0300

    20 Aug 2025

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,6 +32,9 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
+
+        //dd('print');
+
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
